82ce290e4123adc0209ab901b9daa910e01ae74c
mike.bskim <mike.bskim@gmail.com>
2021-08-11 00:21:48 +0200

    bottomNavigationBar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sns_login/src/controller/login_user_controller.dart';
 import 'package:sns_login/src/pages/login_page.dart';
+import 'package:sns_login/src/pages/tab_page.dart';
 
 class App extends StatelessWidget {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
   final LoginUserController _loginUserCtrl = Get.put(LoginUserController());
-
-//    print('metadata: ' + snapshot.data.metadata.toString());
-//    print('metadata: ' + snapshot.data.metadata.lastSignInTime.toString());
 
   @override
   Widget build(BuildContext context) {
@@ -22,39 +17,12 @@ class App extends StatelessWidget {
           return Center(child: CircularProgressIndicator());
         } else {
           if (snapshot.hasData) {
-//            print(snapshot.data);
             print('providerData: ' + snapshot.data.providerData[0].providerId.toString());
             _loginUserCtrl.mappingUserInfo(snapshot);
 
             return Scaffold(
-                appBar: AppBar(
-                  title: Text('Login'),
-                  centerTitle: true,
-                  actions: <Widget>[
-                    IconButton(
-                        icon: Icon(Icons.exit_to_app, color: Colors.white,),
-                        onPressed: () {
-                          FirebaseAuth.instance.signOut();
-                          _googleSignIn.signOut();
-                          FacebookAuth.instance.logOut();
-                        })
-                  ],
-                ),
-                body: Center(
-                  child: Obx(() =>
-                  _loginUserCtrl.curUser.providerId != null
-                  ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('provider ID: ${_loginUserCtrl.curUser.providerId}',
-                        style: TextStyle(fontSize: 20),),
-                      SizedBox(height: 20,),
-                      Text('User UID: ${_loginUserCtrl.curUser.uid}', //snapshot.data.uid,
-                        style: TextStyle(fontSize: 18),),
-                    ],
-                  )
-                  : Center(child: CircularProgressIndicator()) ),
-                ));
+                body: TabPage(),
+            );
           }
           return LoginPage();
         }
